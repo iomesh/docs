21049df2ed370b5c9f2ce6e9406e32d8d0937d0c
fanyang <fanyang@smartx.com>
2020-10-20 20:23:48 +0800

    Fix wrong URL settings

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: "IOMesh", // Title for your website.
   tagline: "A website for IOMesh",
-  url: "https://iomesh.com", // Your website URL
+  url: "http://iomesh.com/docs", // Your website URL
   baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
