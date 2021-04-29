b29adc98b069cf61a63f5b6be1b2f2082e5ade5c
Paula Meng <paula@smartx.com>
2021-04-29 16:32:26 +0800

    Refine
    refine footer and favicon

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -18,45 +18,47 @@ class Footer extends React.Component {
   render() {
     return (
       <footer id="footer" className="nav-footer">
-        <div className="">IOMesh</div>
-        <div className="linkList">
-          <a className="footerLink" href="">Spec</a>
-          <a
-            className="footerLink"
-            href="http://iomesh.com/docs/next/about-iomesh/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Docs
-          </a>
-          <a className="footerLink" href="">Blog</a>
-          <a className="footerLink" href="">Contact Us</a>
-          <a className="footerLink" href="">Privacy Policy</a>
-          <span className=""> | </span>
-          <a
-            className="imgLink"
-            href="https://join.slack.com/t/slack-vcm1551/shared_invite/zt-nx33ud5h-~0D_MD5kL0sUrwslB~KF_A"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="../img/Slack_footer.svg" width={16} height={16} />
-          </a>
-          <a
-            className="imgLink"
-            href="https://twitter.com/iomeshhq"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="../img/Twitter_footer.svg" width={16} height={16} />
-          </a>
-          <a
-            className="imgLink"
-            href="https://www.youtube.com/channel/UCJV59ZDxjm822LK_oUhi7qA"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="../img/YouTube_footer.svg" width={16} height={16} />
-          </a>
+        <div className="footerWrapper">
+          <div className="">IOMesh</div>
+          <div className="linkList">
+            <a className="footerLink" href="">Spec</a>
+            <a
+              className="footerLink"
+              href="http://iomesh.com/docs/next/about-iomesh/introduction"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Docs
+            </a>
+            <a className="footerLink" href="">Blog</a>
+            <a className="footerLink" href="">Contact Us</a>
+            <a className="footerLink" href="">Privacy Policy</a>
+            <span className=""> | </span>
+            <a
+              className="imgLink"
+              href="https://join.slack.com/t/slack-vcm1551/shared_invite/zt-nx33ud5h-~0D_MD5kL0sUrwslB~KF_A"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="../img/Slack_footer.svg" width={16} height={16} />
+            </a>
+            <a
+              className="imgLink"
+              href="https://twitter.com/iomeshhq"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="../img/Twitter_footer.svg" width={16} height={16} />
+            </a>
+            <a
+              className="imgLink"
+              href="https://www.youtube.com/channel/UCJV59ZDxjm822LK_oUhi7qA"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="../img/YouTube_footer.svg" width={16} height={16} />
+            </a>
+        </div>
       </div>
     </footer>
     );
