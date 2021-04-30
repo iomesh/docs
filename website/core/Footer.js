deb7c04ea710e3b572803d0aa9a7148a33c4fb7a
Paula Meng <paula@smartx.com>
2021-04-30 11:19:30 +0800

    fix footer issues

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -21,42 +21,42 @@ class Footer extends React.Component {
         <div className="footerWrapper">
           <div className="">IOMesh</div>
           <div className="linkList">
-            <a className="footerLink" href="">Spec</a>
+            <a className="footerLink" href="https://iomesh.com/spec">Spec</a>
             <a
               className="footerLink"
-              href="http://iomesh.com/docs/next/about-iomesh/introduction"
+              href="http://doc.iomesh.com/next/about-iomesh/introduction"
               target="_blank"
               rel="noreferrer"
             >
               Docs
             </a>
-            <a className="footerLink" href="">Blog</a>
-            <a className="footerLink" href="">Contact Us</a>
-            <a className="footerLink" href="">Privacy Policy</a>
+            {/* <a className="footerLink" href="">Blog</a> */}
+            <a className="footerLink" href="https://iomesh.com/contact">Contact Us</a>
+            <a className="footerLink" href="https://iomesh.com/privacy">Privacy Policy</a>
             <span className=""> | </span>
             <a
-              className="imgLink"
+              className="footerLink"
               href="https://join.slack.com/t/slack-vcm1551/shared_invite/zt-nx33ud5h-~0D_MD5kL0sUrwslB~KF_A"
               target="_blank"
               rel="noreferrer"
             >
-              <img src="../img/Slack_footer.svg" width={16} height={16} />
+              Slack
             </a>
             <a
-              className="imgLink"
+              className="footerLink"
               href="https://twitter.com/iomeshhq"
               target="_blank"
               rel="noreferrer"
             >
-              <img src="../img/Twitter_footer.svg" width={16} height={16} />
+              Twitter
             </a>
             <a
-              className="imgLink"
+              className="footerLink"
               href="https://www.youtube.com/channel/UCJV59ZDxjm822LK_oUhi7qA"
               target="_blank"
               rel="noreferrer"
             >
-              <img src="../img/YouTube_footer.svg" width={16} height={16} />
+              YouTube
             </a>
           </div>
         </div>
