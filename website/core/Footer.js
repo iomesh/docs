afd402b7791fdf03e55c4d3170edbe0c91aaf18d
ziyin.lu <ziyin.lu@smartx.com>
2023-05-11 05:21:37 -0400

    update Footer.js

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -24,7 +24,7 @@ class Footer extends React.Component {
             <a className="footerLink" href="https://iomesh.com/spec">Spec</a>
             <a
               className="footerLink"
-              href="http://docs.iomesh.com/next/about-iomesh/introduction"
+              href="http://docs.iomesh.com/next/introduction/introduction"
               target="_blank"
               rel="noreferrer"
             >
